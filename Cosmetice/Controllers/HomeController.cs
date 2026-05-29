87ecdcd0ff3f1f785c1cf6b6323bbe3aace2de21
Hanie <Hanie@HANNAHLEAH>
2026-05-29 12:31:15 +0200

    getting some ideas for the front end...

--- Cosmetice/Controllers/HomeController.cs
+++ Cosmetice/Controllers/HomeController.cs
@@ -1,21 +1,55 @@
-using System.Diagnostics;
 using Cosmetice.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 
 namespace Cosmetice.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly CosmeticeContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, CosmeticeContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(string searchString, int? pageNumber)
         {
-            return View();
+            ViewData["CurrentFilter"] = searchString;
+
+            var items = from s in _context.Products.Include(p => p.Category).Include(p => p.Brand).Include(p => p.ProductImages)
+                        select s;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                items = items.Where(s =>
+                    (s.Brand != null && s.Brand.Name.Contains(searchString)) ||
+                    (s.Category != null && s.Category.Name.Contains(searchString)) 
+                );
+            }
+
+     
+
+            // pagination
+            // items per page
+            int pageSize = 6;
+            int pageIndex = pageNumber ?? 1;
+
+            var pagedItems = await items
+                .AsNoTracking()
+                .Skip((pageIndex - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            // total count for page buttons
+            int totalItems = await items.CountAsync();
+            ViewBag.TotalPages = (int)Math.Ceiling(totalItems / (double)pageSize);
+            ViewBag.CurrentPage = pageIndex;
+
+            return View(pagedItems);
         }
 
         public IActionResult Privacy()
